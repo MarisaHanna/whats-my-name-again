71cb0281c9da87d4d0a8c392e0c6afd1d74a0dff
Marisa Hanna <m.e.hanna1983@hotmail.com>
2021-03-07 20:14:48 -0800

    finishing degug

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 let today = [
 
     {
-        column:'0',
+        id:'0',
         hour:'8',
         time:'08',
         when:'am',
@@ -9,7 +9,7 @@ let today = [
     },
 
     {
-        column:'1',
+        id:'1',
         hour:'9',
         time:'09',
         when:'am',
@@ -17,7 +17,7 @@ let today = [
     },
 
     {
-        column:'2',
+        id:'2',
         hour:'10',
         time:'10',
         when:'am',
@@ -25,7 +25,7 @@ let today = [
     },
 
     {
-        column:'3',
+        id:'3',
         hour:'11',
         time:'11',
         when:'am',
@@ -33,7 +33,7 @@ let today = [
     },
 
     {
-        column:'4',
+        id:'4',
         hour:'12',
         time:'12',
         when:'pm',
@@ -41,7 +41,7 @@ let today = [
     },
 
     {
-        column:'5',
+        id:'5',
         hour:'1',
         time:'13',
         when:'pm',
@@ -49,7 +49,7 @@ let today = [
     },
 
     {
-        column:'6',
+        id:'6',
         hour:'2',
         time:'14',
         when:'pm',
@@ -57,7 +57,7 @@ let today = [
     },
 
     {
-        column:'7',
+        id:'7',
         hour:'3',
         time:'15',
         when:'pm',
@@ -65,7 +65,7 @@ let today = [
     },
 
     {
-        column:'8',
+        id:'8',
         hour:'4',
         time:'16',
         when:'pm',
@@ -73,13 +73,21 @@ let today = [
     },
 
     {
-        column:'9',
+        id:'9',
         hour:'5',
         time:'17',
         when:'pm',
         reminder:''
     },
     
+
+    {
+        id:'10',
+        hour:'6',
+        time:'18',
+        when:'pm',
+        reminder:''
+    },
 
 
 ]
@@ -92,16 +100,17 @@ function headerDate() {
 function saveDate() {
 
     localStorage.setItem('today', JSON.stringify(today));
-    console.log('now')
 
 }
 
 function myReminders() {
 
     today.forEach(function(thisTime){
-        $(`#${thisTime.column}`).val(thisTime.reminder);
-       console.log('hello')
+
+        $(`#${thisTime.id}`).val(thisTime.reminder);
+
     })
+
 }
 
 
@@ -115,6 +124,7 @@ function init() {
     saveDate();
     myReminders();
 
+  
 }
 
 headerDate();
@@ -139,7 +149,7 @@ let eachHour = $('<div>').attr({
 
 let mySchedule = $('<textarea>');
     eachHour.append(mySchedule);
-    mySchedule.attr('column', thisTime.column);
+    mySchedule.attr('id', thisTime.id);
    
     if (thisTime.time < moment().format('HH')){
         mySchedule.attr({
@@ -172,19 +182,20 @@ let mySchedule = $('<textarea>');
 })
 
 init();
+
 console.log(init)
    
 $('.saveBtn').on('click', function(event){
     event.preventDefault();
+
+
+     let saveContent = $(this).siblings('.description').children('.future').attr('id');
     
-    let saveContent = $(this).siblings('.description').children('.future').attr('column');
-    console.log(saveContent)
+       today[saveContent].reminder = $(this).siblings('.description').children('.future').val();
+      
     
-    today[saveContent].reminder = $(this).siblings('.description').children('.future').val();
-    console.log(saveContent)
-    
-    saveDate();
-    myReminders();
+     saveDate();
+     myReminders();
     
 })
 
